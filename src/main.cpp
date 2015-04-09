026e0fe3a7f49dbc63b9cf0b3fb4a5afbc55374c
Pepi Dimi <pepi55@live.com>
2015-04-09 17:22:46 +0200

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <iostream>
+#include <bitset>
 
 int main(void) {
-	int stuff = 10;
-
-	stuff ^= 22;
-
-	fprintf(stdout, "%i\n", stuff);
+	fprintf(stdout, "size of char %i\n", sizeof(char));
+	fprintf(stdout, "size of int %i\n", sizeof(int));
+	fprintf(stdout, "size of short int %i\n", sizeof(short int));
+	fprintf(stdout, "size of long int %i\n", sizeof(long int));
+	fprintf(stdout, "size of float %i\n", sizeof(float));
+	fprintf(stdout, "size of double %i\n", sizeof(double));
+	fprintf(stdout, "size of wchar_t %i\n", sizeof(wchar_t));
 
 	return 0;
 }
