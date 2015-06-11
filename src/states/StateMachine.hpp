89e0971346fc12b91855e1668a015f2c46faace5
Pepi Dimi <pepi55@live.com>
2015-06-11 14:29:19 +0200

    it wasnt the .tpp

--- src/states/StateMachine.hpp
+++ src/states/StateMachine.hpp
@@ -2,82 +2,30 @@
 #define STATE_MACHINE_HPP
 
 #include <string.h>
-#include <typeinfo>
 
 #include "State.hpp"
 #include "../messages/Telegram.hpp"
 
-template <class entityType> class State;
-
 template <class entityType> class StateMachine {
 	public:
-		StateMachine (entityType *owner) : mOwner(owner), mCurrentState(NULL), mPreviousState(NULL), mGlobalState(NULL) {
-		}
-
-		~StateMachine (void) {
-			delete mOwner,
-						 mCurrentState,
-						 mPreviousState,
-						 mGlobalState;
-		}
+		StateMachine (entityType *owner);
+		~StateMachine (void);
 
 		//Use this to initialize the StateMachine
-		void setCurrentState (State<entityType> *state) {
-			mCurrentState = state;
-		}
+		void setCurrentState (State<entityType> *state);
+		void setPreviousState (State<entityType> *state);
+		void setGlobalState (State<entityType> *state);
 
-		void setPreviousState (State<entityType> *state) {
-			mPreviousState = state;
-		}
+		State<entityType> *getCurrentState (void) const;
+		State<entityType> *getPreviousState (void) const;
+		State<entityType> *getGlobalState (void) const;
 
-		void setGlobalState (State<entityType> *state) {
-			mGlobalState = state;
-		}
-
-		State<entityType> *getCurrentState (void) const {
-			return mCurrentState;
-		}
-
-		State<entityType> *getPreviousState (void) const {
-			return mPreviousState;
-		}
-
-		State<entityType> *getGlobalState (void) const {
-			return mGlobalState;
-		}
-
-		void update (void) const {
-			//Remember to assign Global state on object creation!
-			if (mGlobalState) {
-				mGlobalState->execute(mOwner);
-			}
-
-			//Remember to assign Current state on object creation!
-			if (mCurrentState) {
-				mCurrentState->execute(mOwner);
-			}
-		}
-
-		void changeState (State<entityType> *newState) {
-			if (newState == NULL) {
-				fprintf(stderr, "%s\n", "Changing to a NULL state!");
-			} else {
-				mPreviousState = mCurrentState;
-				mCurrentState->exit(mOwner);
-
-				mCurrentState = newState;
-				mCurrentState->enter(mOwner);
-			}
-		}
-
-		void revertState (void) {
-			changeState(mPreviousState);
-		}
+		void update (void) const;
+		void changeState (State<entityType> *newState);
+		void revertState (void);
 
 		//Returns true if parameter is equal to the current state
-		bool isInState (const State<entityType> &state) const {
-			return typeid(*mCurrentState) == typeid(state);
-		}
+		bool isInState (const State<entityType> &state) const;
 
 	private:
 		entityType *mOwner;
@@ -86,5 +34,7 @@ template <class entityType> class StateMachine {
 		State<entityType> *mPreviousState;
 		State<entityType> *mGlobalState;
 };
+
+#include "StateMachine.tpp"
 
 #endif
